f79b12be493f2045cceb432c2f672c8e99042fae
Derek <derekeklund32@gmail.com>
2024-07-03 08:32:21 -0400

    Watchlist ready to add favorites to database. Saving before database manipulation

--- flaskr/stocks.py
+++ flaskr/stocks.py
@@ -20,7 +20,7 @@ from flaskr.auth import login_required
 def watchlist():
     print("watchlist")
 
-    tickers = ['SPY', 'QQQ', 'NVDA', 'AAPL', 'AMZN', 'MSFT', 'AAPL', 'TSLA', 'META']
+    tickers = ['SPY', 'QQQ', 'NVDA']
 
     prices_dict = {}
 
@@ -29,15 +29,41 @@ def watchlist():
         # Get SPY stock price
         prices = yf.Ticker(t)
 
-        # Get previous day's stock prices
-        historical_prices = prices.history(period="1d", interval="15m")
+        # Get previous couple day's prices
+        historical_prices = prices.history(period="5d", interval="1d")
 
-        # Get latest price and time (PM format)
+        # Get latest price, % change, and time (AM/PM format)
         latest_price = historical_prices['Close'].iloc[-1].round(2)
+        day_before = historical_prices['Close'].iloc[-2].round(2)
+
+        change = ((latest_price - day_before) * 100 / day_before).round(2)
+
         time = historical_prices.index[-1].strftime('%I:%M %p')
 
-        # Add ticker & price to dictionary
-        prices_dict[t] = latest_price
+        # Add price and percent change to nested dictionary
+        prices_dict[t] = {}
+        prices_dict[t]['price'] = latest_price
+        prices_dict[t]['change'] = change
+
+    if request.method == 'GET':
+        print("GET method")
 
 
-    return render_template('stocks/watchlist.html', prices_dict=prices_dict, time=time)
+        return render_template('stocks/watchlist.html', prices_dict=prices_dict, time=time)
+    
+    if request.method == 'POST':
+        print("POST method")
+
+        # Get the ticker that was clicked on from the form
+        added_ticker = request.form['ticker']
+
+        # Remove the + sign and whitespace
+        added_ticker = added_ticker.replace("+", "").strip()
+
+        print("added_ticker: ", added_ticker)
+
+        # Add the ticker to user tables favorites column in database
+
+
+        return render_template('stocks/watchlist.html', prices_dict=prices_dict, time=time, added_ticker=added_ticker)
+
